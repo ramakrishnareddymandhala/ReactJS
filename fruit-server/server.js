eb9ff0aed1df1f57bcdd42f6c71594abbcaa98ab
Rama Krishna Mandhala <ramakrishnareddymandhala@gmail.com>
2021-06-18 18:17:11 +0200

    Implemented Get API to get items from Mongodb

---
+++ fruit-server/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongodb = require("mongodb");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+
+//crate the rest object
+const app = express();
+
+
+
+app.use(express.json());
+
+
+app.use(express.urlencoded({extended:false}));
+
+
+app.use(cors());
+
+const dbClient = mongodb.MongoClient;
+
+app.get("/products",(req,res)=>{
+    dbClient.connect("mongodb+srv://admin:admin@cluster0.gb2ri.mongodb.net/miniproject?retryWrites=true&w=majority",(err,connection)=>{
+        if(err) throw err;
+        else {
+            const db= connection.db("miniproject");
+            db.collection("products").find().toArray((err,array)=>{
+                if(err) throw err;
+                else {
+                    res.send(array);
+                }
+            });
+        }
+    }
+    );
+});
+
+let port = process.env.PORT || 8080;
+app.listen(port,()=>{
+    console.log("seever ready");
+});
